e3f0f4bdf3109ee8b3309db4dc76c77a4ace9385
Mr.Kashan <kashisial2327@gmail.com>
2024-11-14 21:21:51 -0800

    Form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,74 +3,95 @@ import { Input } from "./ui/input";
 import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 
+const formSchema = z.object({
+  username: z.string().min(3).max(50).nonempty("username is required"),
+  email: z
+    .string()
+    .email("invalid email address")
+    .nonempty("email is required"),
+  password: z
+    .string()
+    .min(6, "password must be atleast 6 characters")
+    .nonempty("password is required"),
+});
 const Login = () => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
-  } = useForm();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
   const onSubmit = (data) => {
     console.log(data);
-    localStorage.setItem("user", JSON.stringify(data));
     navigate("/dashboard");
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="w-[100%] md:w-[60%] p-14 h-[500px] m-auto mt-10"
-    >
-      <Input
-        type="text"
-        placeholder="Enter Name"
-        {...register("username", {
-          required: { value: true, message: "Name is required" },
-        })}
-      />
-      {errors.username && (
-        <div style={{ color: "red" }}>{errors.username.message}</div>
-      )}
-
-      <Input
-        type="email"
-        className="mt-5"
-        placeholder="Enter Email"
-        {...register("email", {
-          required: { value: true, message: "Email is required" },
-          pattern: {
-            value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-            message: "Invalid email address",
-          },
-        })}
-      />
-      {errors.email && (
-        <div style={{ color: "red" }}>{errors.email.message}</div>
-      )}
-
-      <Input
-        type="password"
-        className="mt-5 mb-5"
-        placeholder="Enter Password"
-        {...register("password", {
-          required: { value: true, message: "Password is required" },
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters long",
-          },
-        })}
-      />
-      {errors.password && (
-        <div style={{ color: "red" }}>{errors.password.message}</div>
-      )}
-
-      <Button type="submit" disabled={isSubmitting}>
-        {isSubmitting ? "Submitting..." : "Login"}
-      </Button>
-    </form>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 w-[90%] md:w-[40%] m-auto mt-20 border p-5"
+      >
+        <h1 className="text-center text-3xl font-semibold text-red-950 underline underline-offset-8">
+          Login Form
+        </h1>
+        <FormField
+          name="username"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="email"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="password"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 };
 
